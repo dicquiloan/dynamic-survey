5d49de412e24f1cf1ab298c481188869cae421e3
Brian Smith <brianrsmith080@gmail.com>
2020-11-20 01:12:46 +0000

    blah

--- LApoll/algorithms/knn.py
+++ LApoll/algorithms/knn.py
@@ -1,4 +1,5 @@
-from ..models import *
+from  ..models import *
+import numpy
 
 
 def avgQuestionAnswer(questionID):
@@ -14,3 +15,27 @@ def avgQuestionRating(questionID):
 	for qr in questionResponseList:
 		total = total + qr.rating
 	return total / len(questionResponseList)
+
+def dumpObject(inObj):
+	for property, value in vars(inObj).items():
+		print(property, ":", value)
+
+'''
+	for each participant
+		for each question Id 
+			get question response answer
+
+WE NEED TO BE REALLY CAREFUL ABOUT THE ORDERING OF QUERY RESULTS TO MATCH EVERYTHING UP
+'''
+
+def getQuestionResponseAnswersAsFeaturesArray():
+	featuresList = []
+	for participant in Participant.objects.all():
+		featuresList.append([qr.answer for qr in QuestionResponse.objects.filter(participant__id = participant.id)])
+	return numpy.array(featuresList)
+
+def getLivesInLAAsTargetArray():
+	return numpy.array([p.livesInLA for p in Participant.objects.all()])
+
+def getQuestionResponseAnswersFromParticipant(pid):
+	return numpy.array(qr.answer for qr in QuestionResponse.objects.filter(participant__id = pid)) 
