e0f668d35593df534da13740887b6b8755fc8282
Brian Smith <brianrsmith080@gmail.com>
2020-11-24 06:54:55 +0000

    fixed created algorithms.utils, fixed all utility functions to have ranged variants. added knn model generator and predictor

--- LApoll/algorithms/__init__.py
+++ LApoll/algorithms/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['knn', 'rbm', 'svm', 'lr']
+__all__ = ['knn', 'rbm', 'svm', 'lr', 'utils']
--- LApoll/algorithms/knn.py
+++ LApoll/algorithms/knn.py
@@ -1,48 +1,18 @@
-from  ..models import *
-import numpy
+from sklearn.neighbors import KNeighborsClassifier
+from .utils import *
+
+def generateKNNModel(minParticipantID, maxParticipantID):
+	X = answersAsFeaturesArray(minParticipantID, maxParticipantID)
+	y = livesInLAAsTargetArray(minParticipantID, maxParticipantID)
+	#k is 5 by default
+	knnClassifier = KNeighborsClassifier()
+	knnClassifier.fit(X, y)
+
+	return knnClassifier
 
 
-def avgQuestionAnswer(questionID):
-	total = 0
-	questionResponseList = QuestionResponse.objects.filter(question__id = questionID)
-	for qr in questionResponseList:
-		total = total + qr.answer
-	return total / len(questionResponseList)
+def generateKNNCategorizationProbability(minParticipantID, maxParticipantID, participantID):
+	knnModel = generateKNNModel(minParticipantID, maxParticipantID)
+	return knnModel.predict_proba([answersFromParticipant(participantID)])[0][1]
 
-def avgQuestionRating(questionID):
-	total = 0
-	questionResponseList = QuestionResponse.objects.filter(question__id = questionID)
-	for qr in questionResponseList:
-		total = total + qr.rating
-	return total / len(questionResponseList)
 
-def dumpObject(inObj):
-	for property, value in vars(inObj).items():
-		print(property, ":", value)
-
-'''
-	for each participant
-		for each question Id 
-			get question response answer
-
-WE NEED TO BE REALLY CAREFUL ABOUT THE ORDERING OF QUERY RESULTS TO MATCH EVERYTHING UP
-'''
-
-def getQuestionResponseAnswersAsFeaturesArray():
-	featuresList = []
-	for participant in Participant.objects.all():
-		featuresList.append([qr.answer for qr in QuestionResponse.objects.filter(participant__id = participant.id)])
-	return numpy.array(featuresList)
-
-def getLivesInLAAsTargetArray():
-	return numpy.array([p.livesInLA for p in Participant.objects.all()])
-
-def getQuestionResponseAnswersFromParticipant(pid):
-	return numpy.array(qr.answer for qr in QuestionResponse.objects.filter(participant__id = pid)) 
-
-def getQuestionResponseRatingsForQuestion(qid):
-	return [qr.rating for qr in QuestionResponse.objects.filter(question__id = qid)]
-
-def getAverageQuestionResponseRatingForQuestion(qid):
-	ratingsForQuestion = getQuestionResponseRatingsForQuestion(qid)
-	return 
