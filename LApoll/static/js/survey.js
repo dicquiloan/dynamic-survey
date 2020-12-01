73747f29421caf9aa8e680ce0c6f7e7adefccf20
Brian Smith <brianrsmith080@gmail.com>
2020-12-01 07:07:04 +0000

    survey question rows now have border when hovered

---
+++ LApoll/static/js/survey.js
@@ -0,0 +1,4 @@
+/*
+document.querySelectorAll('.questionRow').forEach(x=> x.addEventListener('mousemove', (event)=>event.target.style.scale=2))
+document.querySelectorAll('.questionRow').forEach(x=> x.addEventListener('mouseout', (event)=>event.target.style.scale=1))
+*/
